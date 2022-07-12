ecef9b5cfcf65867178c11594c60d2fbf41fc530
Maxim Krasinkiy <krasinskij96@inbox.ru>
2022-07-12 13:59:28 +0300

    add website

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.title = "Marvel Group";
+
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:true,
+        slidesToShow:3,
+        touchMove:true,
+        variableWidth:true,
+    });
+})
